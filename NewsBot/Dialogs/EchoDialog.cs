eb5c6b18cd478fc159e11ac9c486f91b3647d0a2
Volodymyr Mykytiuk <mvlabaratory@gmail.com>
2018-02-03 22:41:13 +0200

    News bot init commit

---
+++ NewsBot/Dialogs/EchoDialog.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+
+
+namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    [Serializable]
+    public class EchoDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterResetAsync,
+                    "Are you sure you want to reset the count?",
+                    "Didn't get that!",
+                    promptStyle: PromptStyle.Auto);
+            }
+            else
+            {
+                String responseText = await GenerateResponse(message.Text);
+                await context.PostAsync(responseText);
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+
+        private async Task<String> GenerateResponse(String requestText)
+        {
+            await Task.Delay(3000);
+            return String.Format("Fuck, you said: {0}!!", requestText);
+        }
+    }
+}
---
+++ NewsBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=bobbythenewsbot-Web-Deploy.pubxml -p:Password=6f3sXuZ9XKbCk9aRsexhLrT4pDAW9mKCcqEyGbL55EwfNCJHE6Y0G7mj7wqj
+
