feb023e58fa0f778188dbf91af2f09ec4f6ac43b
ELEKS-SOFTWARE\volodymyr.mykytiuk <volodymyr.mykytiuk@eleks.com>
2018-02-05 12:29:43 +0200

    Assembly rename

--- NewsBot/Properties/Settings.Designer.cs
+++ NewsBot/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SimpleEchoBot.Properties {
+namespace NewsBot.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
